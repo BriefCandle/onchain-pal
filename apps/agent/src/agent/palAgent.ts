e1b188646cb65f3f0ff67a6bbcd8b9b1e2f3455c
BriefCandle <yufanling@gmail.com>
2025-11-22 16:58:59 -0300

    feat(agent): add agent, using aimo, add palAgent

---
+++ apps/agent/src/agent/palAgent.ts
@@ -0,0 +1,70 @@
+import { LanguageModel } from "ai";
+import { Address, PublicClient, WalletClient } from "viem";
+import { Agent } from "./agent";
+import { MessageStore } from "./store";
+import { createTools } from "../tools";
+import { NetworkComponents } from "@onchain-pal/contract-client";
+import { Entity, getComponentValue } from "@latticexyz/recs";
+import {} from "@onchain-pal/contract-client";
+import { observe } from "./observe";
+import { EvmSmartAccount } from "@coinbase/cdp-sdk";
+
+export class PalAgent extends Agent {
+  constructor({
+    id,
+    model,
+    components,
+    gameAddress,
+    publicClient,
+    walletClient,
+    store,
+  }: {
+    id: number;
+    model: LanguageModel | string;
+    components: NetworkComponents;
+    gameAddress: string;
+    publicClient?: PublicClient;
+    walletClient?: EvmSmartAccount;
+    store?: MessageStore;
+  }) {
+    const agentInfo = getComponentValue(
+      components.TokenData,
+      id.toString() as Entity
+    );
+    if (!agentInfo)
+      throw new Error(`PalAgent: heroInfo not found for heroId ${id}`);
+
+    if (!walletClient) {
+      throw new Error(`PalAgent: walletClient is required for agent ${id}`);
+    }
+
+    const tools = createTools(components, id, walletClient);
+
+    const system =
+      ` call tools to move to a random coordinate with x between 0 and 10000 and y between 0 and 10000`.trim();
+
+    super({
+      id,
+      model,
+      system,
+      store,
+      tools: {
+        move: tools.move,
+        // talk tool is commented out in tools.ts
+        // talk: tools.talk,
+      },
+      observe: async () => {
+        const observations = observe({
+          components,
+          heroId: id,
+        });
+        return [
+          {
+            role: "user",
+            content: observations,
+          },
+        ];
+      },
+    });
+  }
+}
